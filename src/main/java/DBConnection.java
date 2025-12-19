99f4e8f13fab0dad3e574f1975cf2e4f2e0a0937
Mahefa <hei.mahefa.2@gmail.com>
2025-12-19 12:01:45 +0300

    add DBConnection contain

--- src/main/java/DBConnection.java
+++ src/main/java/DBConnection.java
@@ -1,2 +1,16 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class DBConnection {
+    private static final String URL = "jdbc:postgresql://localhost:5432/product_management_db";
+    private static final String USER = "product_manager_user";
+    private static final String PASSWORD = "123456";
+
+    public static Connection getDBConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (Exception e) {
+            throw new RuntimeException("Cannot connect to database", e);
+        }
+    }
 }
