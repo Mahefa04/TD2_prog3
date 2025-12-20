4f4ca0c42bffb68a206e7eb6e56315b9c091d884
Mahefa <hei.mahefa.2@gmail.com>
2025-12-20 05:54:57 +0300

    modification of DBConection contain

--- src/main/java/DBConnection.java
+++ src/main/java/DBConnection.java
@@ -2,13 +2,17 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DBConnection {
-    private static final String URL = "jdbc:postgresql://localhost:5432/product_management_db";
-    private static final String USER = "product_manager_user";
-    private static final String PASSWORD = "123456";
+    private static final String DB_URL = System.getenv("DB_URL");
+    private static final String DB_USER = System.getenv("DB_USER");
+    private static final String DB_PASSWORD = System.getenv("DB_PASSWORD");
 
     public static Connection getDBConnection() {
+        if (DB_URL == null || DB_USER == null || DB_PASSWORD == null) {
+            throw new RuntimeException("lack of variableEnvironment : " +
+                    "DB_URL, DB_USER AND DB_PASSWORD");
+        }
         try {
-            return DriverManager.getConnection(URL, USER, PASSWORD);
+            return DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
         } catch (Exception e) {
             throw new RuntimeException("Cannot connect to database", e);
         }
